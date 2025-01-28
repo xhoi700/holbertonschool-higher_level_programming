633a8af0f322f4ab6b21229bcfe51d993b2d0a69
xhoi700 <10057@holbertonstudents.com>
2025-01-28 12:08:15 +0000

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+    We will create a class named Square
+'''
+
+
+class Square:
+    '''
+        It takes 1 arguments
+    '''
+    def __init__(self, size):
+        self.__size = size
