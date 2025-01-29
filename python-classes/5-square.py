f07c0923e9e42376641c3a83e938afde6cd05d8b
xhoi700 <10057@holbertonstudents.com>
2025-01-29 11:43:55 +0000

    add

---
+++ python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+"""module about a class in python"""
+
+
+class Square:
+    """the class Square"""
+    def __init__(self, size=0):
+        """constructor of the object"""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """func to calculate area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """getter of the property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of the value of an attribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
