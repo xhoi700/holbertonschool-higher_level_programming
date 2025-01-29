1127aa80f18cee43df65c159db62e496063af016
xhoi700 <10057@holbertonstudents.com>
2025-01-29 17:24:56 +0000

    5-rectangle.py

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -6,8 +6,8 @@
 class Rectangle:
     """Class for a rectangle"""
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -34,26 +34,26 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.__width * self.__height
+        return self.__height * self.__width
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return 2 * (self.__height + self.__width)
 
     def __str__(self):
-        rect = ""
         if self.__width == 0 or self.__height == 0:
-            return rect
+            return ""
+        new_str = ""
         for i in range(self.__height):
             for j in range(self.__width):
-                rect += "#"
+                new_str += "#"
             if i < self.__height - 1:
-                rect += "\n"
-        return rect
+                new_str += "\n"
+        return new_str
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
-    
+
     def __del__(self):
-        print(f"Bye rectangle...")
+        print("Bye rectangle...")
