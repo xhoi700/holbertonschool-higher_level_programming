885ee8fed93194feee10ce33072e785b1794a253
xhoi700 <10057@holbertonstudents.com>
2025-02-04 11:17:21 +0000

    add

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Module for a class"""
+
+
+class BaseGeometry:
+    """Class of basegeometry"""
+    def area(self):
+        """Func to calculate area"""
+        raise Exception("area() is not implemented")
