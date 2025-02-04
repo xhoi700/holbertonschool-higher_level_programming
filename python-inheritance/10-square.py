63c00977272540fb44e25e42e2f5293bc223b382
xhoi700 <10057@holbertonstudents.com>
2025-02-04 11:44:08 +0000

    9-rectangle.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module about a square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class that implements a square"""
+    def __init__(self, size):
+        Rectangle.integer_validator(self, "size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Func to calculate area"""
+        return self.__size ** 2
