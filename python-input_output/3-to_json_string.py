ea9edfc52a6ac8e81a1f2c9ea28dcca95c37997c
xhoi700 <10057@holbertonstudents.com>
2025-02-11 11:56:31 +0000

    3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""Module that contains the function json_file."""
+
+"""module to convert to json"""
 
 import json
+
+
 def to_json_string(my_obj):
-    '''func that converts obj to json str'''
+    """func to convert to json"""
     return json.dumps(my_obj)
