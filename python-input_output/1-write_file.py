2b73f09978823eb3396447589d9e5e80dbf7427a
xhoi700 <10057@holbertonstudents.com>
2025-02-11 11:15:13 +0000

    1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""Module that contains the function write_file."""
+
+
+def write_file(filename="", text=""):
+    """write a text file (UTF8) and prints it to stdout."""
+    with open(filename, "w", encoding="UTF8") as f:
+        content = f.write(text)
+        return contains
