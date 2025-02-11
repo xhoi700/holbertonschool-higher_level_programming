3bd194e0f491dba26aa646408ad20a4140f27745
xhoi700 <10057@holbertonstudents.com>
2025-02-11 11:11:49 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-''' module about to read a file'''
-def read_file(filename=""):  
-    """
-    Reads a text file and prints its content to the standard output.
-    
-    Parameters:
-    filename (str): The name of the file to read. Defaults to an empty string.
-    """
-    with open(filename, "r", encoding="UTF8") as file:  # Opens the file in read mode with UTF-8 encoding
-        content = file.read()  
-        print(content, end="") 
+
+
+"""Module that contains the function read_file."""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, "r", encoding="UTF8") as f:
+        content = f.read()
+        print(content, end="")
