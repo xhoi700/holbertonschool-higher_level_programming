22139d4d72602078b5636832a5c9e2d0bbf9f7e8
xhoi700 <10057@holbertonstudents.com>
2025-02-12 10:17:27 +0000

    8-class_to_json.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-import os
-from sys import argv
+'''json file'''
 
-"""Imports functions from external scripts to save and load JSON data"""
+import sys
+import os
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-the_list = []
+if os.path.exists('add_item.json'):
+    my_list = load_from_json_file('add_item.json')
+else:
+    my_list = []
 
-if os.path.exists("add_item.json"):
-    the_list = load_from_json_file("add_item.json")
-for n in range(1, len(argv)):
-    the_list.append(argv[n])
-save_to_json_file(the_list, "add_item.json")
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, 'add_item.json')
