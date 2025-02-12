e9fff78ff292ff47e61f9f28deeae2cc52d8e495
xhoi700 <10057@holbertonstudents.com>
2025-02-12 10:12:18 +0000

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,10 +6,10 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-the_list = []  
+the_list = []
 
 if os.path.exists("add_item.json"):
-    the_list = load_from_json_file("add_item.json")  
+    the_list = load_from_json_file("add_item.json")
 for n in range(1, len(argv)):
     the_list.append(argv[n])
 save_to_json_file(the_list, "add_item.json")
