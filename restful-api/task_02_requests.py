f49adca35a76153ff45d673cc4e2b11c50e49603
xhoi700 <10057@holbertonstudents.com>
2025-02-19 09:47:17 +0000

    task_02_requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,33 @@
+import requests
+import csv
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post["title"])
+        else:
+            print("Failed to fetch posts")
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    if response.status_code == 200:
+        posts = response.json()
+        data_to_write = []
+        #for post in posts:
+        #
+        #data_to_write.append({"id": post["id"], "title" : post["title"], "body" : post["body"]})
+        post_data = [{"id": post["id"], "title" : post["title"], "body" : post["body"]} for post in posts]
+    
+    with open("posts.csv", "w", encoding="utf-8") as file:
+        fieldnames = ["id", "title", "body"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+        writer.writeheader()  # Shkruan header-at
+        writer.writerows(post_data)  # Shkruan të dhënat
+
+
