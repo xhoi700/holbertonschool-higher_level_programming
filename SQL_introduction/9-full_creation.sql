ae37df09c6cb355cbe921d20ceb715c586d31876
xhoi700 <10057@holbertonstudents.com>
2025-03-04 10:30:42 +0000

    adedd

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,2 +1,12 @@
--- show full creation
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT) INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10),(2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8)
+-- create a full table
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
