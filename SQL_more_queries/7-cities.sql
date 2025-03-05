c17f7601d408c6b5e098059a5667a67c94106bed
xhoi700 <10057@holbertonstudents.com>
2025-03-05 12:54:45 +0000

    added

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
--- Write a script that creates the database hbtn_0d_usa and the table cities
+-- script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-    id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    states_id INT NOT NULL,
-    name  VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
+	id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id int NOT NULL,
+	name varchar(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
 );
