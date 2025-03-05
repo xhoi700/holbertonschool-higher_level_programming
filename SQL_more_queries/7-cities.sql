43af8e5b20d476446b63a20cc891f1527acdc77a
xhoi700 <10057@holbertonstudents.com>
2025-03-05 12:53:39 +0000

    added

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     states_id INT NOT NULL,
-    name  VARCHAR(256) NOT NULL
+    name  VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
