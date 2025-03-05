0de6b0c147a251c7ae2a1f45d0abeff726476798
xhoi700 <10057@holbertonstudents.com>
2025-03-05 10:36:22 +0000

    added

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---Write a script that lists all privileges of the MySQL users 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- script that lists all privileges of the MySQL users
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
